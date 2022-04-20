5f8b251d2469464bbabf85f857770d19d4385723
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2022-04-20 15:06:11 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiBackgroundServices/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiBackgroundServices.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"{DateTime.UtcNow:o}");
+        }
+    }
+}
