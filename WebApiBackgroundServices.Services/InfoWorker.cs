e5e5c4db180d3c661a5141c6a4436976fc82498a
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2022-04-21 08:16:39 -0300

    Aqui foi criado o serviço e adicionado ao program para ser iniciado na aplicaçao, e a cada servicó criado e chamado dentro de programa ele subirá paralelamente na aplicaçao uma thread

---
+++ WebApiBackgroundServices.Services/InfoWorker.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace WebApiBackgroundServices.Services
+{
+    public class InfoWorker : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Process Started {nameof(InfoWorker)} !!!");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Process Finished {nameof(InfoWorker)} !!!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
