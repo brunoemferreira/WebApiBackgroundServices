45375cb9da2e66ffc2ca4b21aeb66cc7d220061b
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2022-04-21 08:57:08 -0300

    Criação do Timer na infoworker

--- WebApiBackgroundServices.Services/InfoWorker.cs
+++ WebApiBackgroundServices.Services/InfoWorker.cs
@@ -4,11 +4,23 @@ namespace WebApiBackgroundServices.Services
 {
     public class InfoWorker : IHostedService
     {
+        private int executionCount = 0;
+        private Timer _timer = null!;
+        private string message = "Mensagem Inicial....";
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
             Console.WriteLine($"Process Started {nameof(InfoWorker)} !!!");
 
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+            TimeSpan.FromSeconds(1));
+
             return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            Console.WriteLine(message);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
